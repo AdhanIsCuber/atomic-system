c76cc226d4eca273321179cdd7ab0fa0a284de71
raffifu <raffifebriandikau@gmail.com>
2020-10-18 17:29:26 +0700

    add list

---
+++ src/list.ts
@@ -0,0 +1,18 @@
+import { Property as CSS } from 'csstype'
+import { system, Config, ResponsiveValue, ThemeType } from '@styled-system/core'
+
+export interface ListProps<Theme extends ThemeType = ThemeType> {
+	list?: ResponsiveValue<CSS.ListStyle, Theme>
+	listPosi?: ResponsiveValue<CSS.ListStylePosition, Theme>
+	listImage?: ResponsiveValue<CSS.ListStyleImage, Theme>
+	listType?: ResponsiveValue<CSS.ListStyleType, Theme>
+}
+
+const config: Config<ListProps> = {
+	list: {property: 'listStyle'},
+	listPosi: {property: 'listStylePosition'},
+	listImage: {property: 'listStyleImage'},
+	listType: {property: 'listStyleType'},
+}
+
+export const list = system<ListProps>(config)
